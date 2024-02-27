e50838cc1b306c944cfe0e7b5a3c05184c6bcdbe
cmd <cmd@yggdrasil.onion>
2024-02-26 22:10:09 -0600

    update

--- web/src/components/ui/drawer/index.tsx
+++ web/src/components/ui/drawer/index.tsx
@@ -12,7 +12,7 @@ export default function UserView () {
   return (
     <Box 
       bg='white' 
-      h={ signer !== null ? '200px' : '625px'}>
+      h={ signer !== null ? '200px' : '555px'}>
       { signer === null
         && <LoaderView />
         || <SignerView />
